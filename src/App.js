9b92853c50a54de6f90a89ea0d5892549d1fd081
“Ben <bencjacobs6@gmail.com>
2022-08-23 13:54:49 -0400

    start of nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-       hello world
+       <Navbar dark color='primary' sticky='top' expand='md'>
+               <Container>
+                    <NavbarBrand href='/'>
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            hello world
     </div>
   );
 }
