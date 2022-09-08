061ff0ccff3d1ec2ffc91d2c8f0081afac3e969f
“Ben <bencjacobs6@gmail.com>
2022-09-08 15:49:52 -0400

    more react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import MediaPage from './pages/MediaPage';
+import StorePage from './pages/StorePage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/HeaderComponent';
 import Footer from './components/FooterComponent';
 import './App.css';
@@ -12,6 +15,9 @@ function App() {
             <Header />
             <Routes>
                 <Route path='/' element={<HomePage />} />
+                <Route path='media' element={<MediaPage/>} />
+                <Route path='store' element={<StorePage/>} />
+                <Route path='about' element={<AboutPage/>} />
                 <Route path='contact' element={<ContactPage />} />
             </Routes>
             <Footer />
