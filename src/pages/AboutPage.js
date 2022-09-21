f2b7c2cac5a6e6b87a245ba727d56f315f6c2713
“Ben <bencjacobs6@gmail.com>
2022-09-21 13:15:35 -0400

    reactstrap media and about

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,34 +4,39 @@ import { Link } from 'react-router-dom';
 import katyWave from '../assets/katyWave.jpeg'
 
 const AboutPage = () => {
-    return <div classname='container'>
-        <div clasName='row'>
-            <div className='col'>
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/'>Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>About</BreadcrumbItem>
-                </Breadcrumb>
-                <h2> About </h2>
-                <hr />
+    return (
+        <div className='container'>
+            <div clasName='row'>
+                <div className='col'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>About</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2> About </h2>
+                    <hr />
+                </div>
+            </div>
+            <div className='row row-content '>
+                <div className='col '>
+                    <Card style={{ width: '18rem' }}>
+                        <CardImg className='d-block mx-auto img-fluid' width="50%" src={katyWave} alt='katy picture' />
+                        <CardImgOverlay>
+                            <CardTitle >Katy Preston</CardTitle>
+
+                        </CardImgOverlay>
+                    </Card>
+                    <Card>
+                        <CardTitle>About</CardTitle>
+                        <CardBody>Kati Preston: Holocaust & cancer survivor, wife, mother,
+                            grandmother, mentor, fashion designer, model, journalist, entrepreneur, <br></br>
+                            impresario, friend, cook, public speaker and campaigner against hate of
+                            any sort. Born in Transylvania in 1939 she now lives in Barnstead, New Hampshire.
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
         </div>
-        <Card style={{ width: '18rem' }}>
-            <CardImg width="50%" src={katyWave} alt='katy picture' />
-            <CardImgOverlay>
-                <CardTitle>Katy Preston</CardTitle>
-
-            </CardImgOverlay>
-        </Card>
-        <Card>
-            <CardTitle>About</CardTitle>
-            <CardBody>Kati Preston: Holocaust & cancer survivor, wife, mother,
-                grandmother, mentor, fashion designer, model, journalist, entrepreneur, <br></br>
-                impresario, friend, cook, public speaker and campaigner against hate of
-                any sort. Born in Transylvania in 1939 she now lives in Barnstead, New Hampshire.
-            </CardBody>
-        </Card>
-
-    </div>;
+    )
 };
 
 export default AboutPage;
--- src/pages/MediaPage.js
+++ src/pages/MediaPage.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import {
-    Card, CardImg, CardText, CardBody, CardTitle,
     Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const MediaPage = () => {
     return (
-        <div classname='container'>
-            <div classname='row'>
-                <div classname='col'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rXOKU_ne5zw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <br/>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Sn2mhSi6AnQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <br/>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/he-jEexqCLc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Media</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Media</h2>
+                    <hr />
                 </div>
-
+            </div>
+            <div className='row row-content'>
+            <div className='col-md-6'>
+                <div>
+                    <iframe width="560" height="315" src='https://www.youtube.com/embed/rXOKU_ne5zw' type="video/mp4" controls allowFullScreen> Your browser does not support this video.
+                    </iframe>
+                </div>
+                <br />
+                <div >
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Sn2mhSi6AnQ" title="YouTube video player" controls allowfullscreen>Your browser does not support this video.</iframe>
+                </div>
+                <br />
+                <div>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/he-jEexqCLc" title="YouTube video player" controls allowfullscreen>Your browser does not support this video.</iframe>
+                </div>
+            </div>
             </div>
         </div>
     )
