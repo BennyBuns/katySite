ffe7489424987e0372684583d67854bf6a84620a
ben jacobs <bencjacobs6@gmail.com>
2023-01-23 16:27:09 -0600

    styling header img

--- src/components/ImageHeader.js
+++ src/components/ImageHeader.js
@@ -3,11 +3,11 @@ import katiLogo from '../assets/katiLogo.jpg';
 
 const HeaderImg = () => {
     return (
-        <section className='bg-secondary'>
+        <section className='bg-secondary cover'>
             <div className='container'>
                 <div className='row'>
                     <div className='col headerBackground'>
-                        <img className='headerImg' src={katiLogo} height={300} width={600}/>
+                        <img className='img-fluid' src={katiLogo} />
                     </div>
                 </div>
             </div>
